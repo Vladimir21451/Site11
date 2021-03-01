7183d977ed65eead05671c3b2df994fdff90a2c1
Vladimir21451 <76568154+Vladimir21451@users.noreply.github.com>
2021-03-01 20:00:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -76,13 +76,13 @@ document.getElementById('btnEqual').addEventListener('click', function () {
                 document.body.style.backgroundImage='url("калейдоскоп.webp")';
                 break;
             case 1:
-                document.body.style.backgroundImage='url("./minjon.webp")';
+                document.body.style.backgroundImage='url("/minjon.webp")';
                 break;
             case 2:
-                document.body.style.backgroundImage='url("./калейдоскоп.webp")';
+                document.body.style.backgroundImage='url(/калейдоскоп.webp)';
                 break;
             case 3:
-                document.body.style.backgroundImage='url("./триповый.webp")';  
+                document.body.style.backgroundImage='url("/триповый.webp")';  
         }
        // document.body.style.backgroundColor='lightgreen';
        // document.body.style.backgroundImage = 'url(images/калейдоскоп.webp)';
